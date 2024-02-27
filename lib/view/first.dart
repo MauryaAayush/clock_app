fdf34160fd1b400ab2bc45ccd313000ea7164708
MauryaAayush <maayush235@gmail.com>
2024-02-27 19:14:40 +0530

    Radhey radhey

---
+++ lib/view/first.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(const SimpleClock());
+}
+
+class SimpleClock extends StatefulWidget {
+  const SimpleClock({super.key});
+
+  @override
+  State<SimpleClock> createState() => _SimpleClockState();
+}
+
+DateTime dateTime = DateTime.now();
+
+class _SimpleClockState extends State<SimpleClock> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('Digital Clock',style: TextStyle(
+          color: Colors.white,fontWeight: FontWeight.w600
+        ),),
+        centerTitle: true,
+        elevation: 10,
+       shadowColor: Colors.black,
+      ),
+      body: Center(
+        child: Container(
+                color: Colors.black,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('${dateTime.hour % 12} :',style: TextStyle(
+                color: Colors.white,
+                fontSize: 60
+              ),),
+              Text(' ${dateTime.minute} : ',style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 60
+              ),),
+              Text('${dateTime.second}',style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 60
+              ),),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
