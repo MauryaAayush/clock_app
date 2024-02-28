14370c00030e35f8cb4fada7af055112a04d3be0
MauryaAayush <maayush235@gmail.com>
2024-02-28 11:05:18 +0530

    Radhey radhey

--- lib/view/first.dart
+++ lib/view/first.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(const SimpleClock());
 }
 
@@ -16,43 +16,41 @@ class SimpleClock extends StatefulWidget {
 
 DateTime dateTime = DateTime.now();
 
-
 class _SimpleClockState extends State<SimpleClock> {
   @override
   Widget build(BuildContext context) {
-
     int hour = dateTime.hour % 12;
     hour = (hour == 0) ? 12 : hour;
 
-    Timer.periodic(
-      Duration(seconds: 1),
-        (timer){
-        setState(() {
-          dateTime = DateTime.now();
-        });
-        }
-        );
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        dateTime = DateTime.now();
+      });
+    });
 
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text('Digital Clock',style: TextStyle(
-          color: Colors.white,fontWeight: FontWeight.w600
-        ),),
-        centerTitle: true,
-        elevation: 10,
-       shadowColor: Colors.black,
-      ),
-      body: Center(
-        child: Stack(
-            children:[
-              Container(
-                height: double.infinity,
-                width:double.infinity ,
-                  // color: Colors.blue,
-                  child: Image.asset('assets/images/back.jpg',fit: BoxFit.fitHeight,),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            'Digital Clock',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+          ),
+          centerTitle: true,
+          elevation: 10,
+          shadowColor: Colors.black,
+        ),
+        body: Center(
+          child: Stack(children: [
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              // color: Colors.blue,
+              child: Image.asset(
+                'assets/images/back.jpg',
+                fit: BoxFit.fitHeight,
               ),
+            ),
             Positioned(
               top: 350,
               left: 50,
@@ -60,54 +58,60 @@ class _SimpleClockState extends State<SimpleClock> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 // crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-
-                  Container(
-                    height: 100,
-                    width: 80,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: Colors.black.withOpacity(0.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-
-                    ),
-
-                    child: Text('$hour',style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 50,
+                  ClipRect(
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                      child: Container(
+                        height: 100,
+                        width: 80,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.5),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        child: Text(
+                          '$hour',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 50,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                  SizedBox(width: 10,),
-                  Container(
-                    height: 100,
-                    width: 80,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.black.withOpacity(0.5),
-                      borderRadius: BorderRadius.all(Radius.circular(10))
-                    ),
-
-                    child: Text(' ${dateTime.minute} ',style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 50
-                     ),
-                    ),
+                  SizedBox(
+                    width: 10,
                   ),
-                  SizedBox(width: 10,),
                   Container(
                     height: 100,
                     width: 80,
                     alignment: Alignment.center,
                     decoration: BoxDecoration(
                         color: Colors.black.withOpacity(0.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10))
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    child: Text(
+                      ' ${dateTime.minute} ',
+                      style: TextStyle(color: Colors.white, fontSize: 50),
                     ),
-                    child: Text('${dateTime.second}',style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 50
-                    ),),
                   ),
-                  SizedBox(width: 10,),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    height: 100,
+                    width: 80,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.5),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    child: Text(
+                      '${dateTime.second}',
+                      style: TextStyle(color: Colors.white, fontSize: 50),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
                   Padding(
                     padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
                     child: Container(
@@ -116,20 +120,17 @@ class _SimpleClockState extends State<SimpleClock> {
                       alignment: Alignment.center,
                       decoration: BoxDecoration(
                           color: Colors.black.withOpacity(0.5),
-                          borderRadius: BorderRadius.all(Radius.circular(10))
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      child: Text(
+                        "${dateTime.hour < 12 ? 'AM' : 'PM'}",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
                       ),
-                      child: Text("${dateTime.hour < 12 ? 'AM' : 'PM'}",style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25
-                      ),),
                     ),
                   ),
                 ],
               ),
             ),
-        ]
-        ),
-      )
-    );
+          ]),
+        ));
   }
 }
